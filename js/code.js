23b09a497642843445f4964e32b7af43c88db00f
TheJeffRicardo <konkemalindi@gmail.com>
2022-11-25 14:49:50 +0200

    'update'

--- js/code.js
+++ js/code.js
@@ -1,29 +1,45 @@
 let inputValue = document.querySelector("#input")
+let comma = true
 
 // Function for the input display
 function display(number) {
     inputValue.value += number
-    return number
+    comma = true
+}
+function symbol(number) {
+    inputValue.value += number
+    comma = true
 }
 
-// process to solve operation
+// Process to solve operation
 function answer() {
-    inputValue.value = eval(inputValue.value)
+    try{
+        inputValue.value = eval(inputValue.value)
+    }
+    catch(err){
+        inputValue.value = 'Error!!!'
+    }
 }
 
-// clearing the screen
+// Clearing the screen
 function clr() {
-    inputValue.value = " "
+    inputValue.value = inputValue.value.slice(0, 0)
 }
 
-// delete
+// Delete
 function dlt() {
     inputValue.value = inputValue.value.slice(0, -1)
 }
-// function once() {
-//     if{
 
-//     }else{
+// Display comma
+function displayComma(number){
+    if(comma == true){
+        inputValue.value += number
+        comma = false
+    }
+}
 
-//     }
-// }
+// Arrow function for comma
+let toggleComma = () => {
+    comma = true
+}
